e981715be904c433212ade658965a65788e48982
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-08-26 15:46:54 -0300

    Configurando Redux :sparkles:

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,11 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INIT_STATE = {};
+
+const userReducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
